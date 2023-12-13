5aed3a9aa0b5c68632f1ea5038aee33ba8f20566
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-12-13 10:10:29 +0000

    Add day 13 solutions

---
+++ day13/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func timeFunction(function func()) {
+	start := time.Now()
+	function()
+	fmt.Println("Time elapsed:", time.Since(start))
+}
+
+func getColumn(grid []string, n int) string {
+	col := ""
+	for _, row := range grid {
+		col += string(row[n])
+	}
+	return col
+}
+
+func diffStrings(a string, b string) int {
+	diff := 0
+	max := max(len(a), len(b))
+	for i := 0; i < max; i++ {
+		if a[i] != b[i] {
+			diff++
+		}
+	}
+	return diff
+}
+
+func scoreGrid(grid []string) (int, int) {
+	part1, part2 := 0, 0
+	for i := 0; i < len(grid)-1; i++ { // Check rows
+		diff := 0
+		for j := i; j >= 0 && len(grid)+j > 2*i+1; j-- {
+			diff += diffStrings(grid[j], grid[2*i-j+1])
+		}
+		switch diff {
+		case 0:
+			part1 = (i + 1) * 100
+		case 1:
+			part2 = (i + 1) * 100
+		}
+	}
+
+	for i := 0; i < len(grid[0])-1; i++ { // Check columns
+		diff := 0
+		for j := i; j >= 0 && len(grid[0])+j > 2*i+1; j-- {
+			diff += diffStrings(getColumn(grid, j), getColumn(grid, 2*i-j+1))
+		}
+		switch diff {
+		case 0:
+			part1 = i + 1
+		case 1:
+			part2 = i + 1
+		}
+	}
+	return part1, part2
+}
+
+func solve() {
+	raw, err := os.ReadFile("input.txt") // Read file
+	check(err)
+	sections := strings.Split(strings.TrimSpace(string(raw)), "\n\n")
+
+	part1, part2 := 0, 0
+	for _, section := range sections {
+		grid := strings.Split(strings.TrimSpace(section), "\n")
+		a, b := scoreGrid(grid)
+		part1 += a
+		part2 += b
+	}
+
+	fmt.Println("Part 1:", part1)
+	fmt.Println("Part 2:", part2)
+}
+
+func main() {
+	timeFunction(solve)
+}
